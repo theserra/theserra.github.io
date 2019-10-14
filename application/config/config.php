db5617d500a9eea86b878e0cff74907f82d10916
ahmadisme <ahmadneverlast@gmail.com>
2019-10-14 23:29:25 +0700

    rubah

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/medi/';
+$config['base_url'] = 'http://theserra.github.io/';
 
 /*
 |--------------------------------------------------------------------------
